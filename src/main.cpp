6f34ae4b79ebae80890ac5338ac603a4b9be2f7a
wengsht <wengsht.sysu@gmail.com>
2014-11-09 22:25:52 +0800

    first

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+
+/*
+ID: wengsht1
+LANG: C++
+TASK: test
+*/
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <map>
+#include <sstream>
+#include <set>
+using namespace std;
+
+#define MX 100005
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define OREP(i,n) for(int i=1;i<=(n);i++)
+
+typedef long long          LL;
+typedef unsigned long long ULL;
+typedef unsigned int       UINT;
+
+int n, m, k, t;
+
+int main() {
+    return 0;
+}
+
